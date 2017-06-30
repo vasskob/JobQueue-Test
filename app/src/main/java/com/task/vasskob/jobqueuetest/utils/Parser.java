edfb1cc41d023facb21a310f17bf9601a8e15b72
vasskob <vasskob@gmail.com>
2017-06-30 18:03:57 +0300

    Bug fix with persisting.

--- app/src/main/java/com/task/vasskob/jobqueuetest/utils/Parser.java
+++ app/src/main/java/com/task/vasskob/jobqueuetest/utils/Parser.java
@@ -1,13 +1,22 @@
 package com.task.vasskob.jobqueuetest.utils;
 
+import android.util.Log;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.task.vasskob.jobqueuetest.model.User;
 
 public class Parser {
 
+    private static final String TAG = Parser.class.getSimpleName();
+
     public static User getParsedUser(String jsonLine) {
-        Gson gson = new GsonBuilder().create();
-        return gson.fromJson(jsonLine, User.class);
+        try {
+            Gson gson = new GsonBuilder().create();
+            return gson.fromJson(jsonLine, User.class);
+        } catch (Exception ex) {
+            Log.e(TAG, "getParsedUser: ", ex);
+        }
+        return null;
     }
 }
